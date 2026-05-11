236d6d01e8ea4584e0d2e583d9f95d2c77391d72
Yo <felipegaviria17@hotmail.com>
2026-05-11 12:02:36 -0500

    Diana Mejor

--- src/app/dianamarulanda/diana-marulanda-client.tsx
+++ src/app/dianamarulanda/diana-marulanda-client.tsx
@@ -152,7 +152,7 @@ export default function DianaMarulandaClient() {
               alt="Diana Marulanda"
               fill
               priority
-              className="object-cover object-top"
+              className="object-cover object-center"
             />
             <div className="diana-card__hero-shade" />
 
